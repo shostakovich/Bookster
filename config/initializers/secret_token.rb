6d5a6eebe5b844bdd54fdc7437be51c2642187e1
Robert Curth <robert@rocu.de>
2012-01-22 12:43:14 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookFlow::Application.config.secret_token = 'aff83e130e3bfb515b039e02668068ca2aa292ab533e9fed9621eb191a5fc76e6558a790cb11853fd088cdafa9ba0a3c612ab2ff53b379283c2f5f74a09db345'
